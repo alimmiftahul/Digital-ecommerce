8b4b4ee13f4f847c14b3491bec46f025d34d825e
alimmiftahul <alimmiftaul08@gmail.com>
2024-06-18 09:10:04 +0700

    add image, and utilities

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -4,7 +4,8 @@ import React, { useState } from "react";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
-  return <div className="flex h-full gap-4">NavItems</div>;
+
+  return <div className="flex h-full gap-4"></div>;
 };
 
 export default NavItems;
